dc5ebb672c69aa4abce6c573388d45e5d584fd29
Victor Sofiischi <v.sofiischi@copybet.com>
2025-10-01 10:49:00 +0300

    intial commit

--- App.tsx
+++ App.tsx
@@ -6,18 +6,16 @@
  */
 
 import { NewAppScreen } from '@react-native/new-app-screen';
-import { StatusBar, StyleSheet, useColorScheme, View } from 'react-native';
+import { StatusBar, StyleSheet, View } from 'react-native';
 import {
   SafeAreaProvider,
   useSafeAreaInsets,
 } from 'react-native-safe-area-context';
 
 function App() {
-  const isDarkMode = useColorScheme() === 'dark';
-
   return (
     <SafeAreaProvider>
-      <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
+      <StatusBar barStyle={'dark-content'} />
       <AppContent />
     </SafeAreaProvider>
   );
